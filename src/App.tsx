5e95934e98a07d14c8bd160df12f49f06dba55c9
dzimentey <dzimentey@gmail.com>
2021-09-21 02:43:45 +0300

    feat: update Instance component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {Skills} from "./skills/Skills";
 import {MyWorks} from "./my-works/MyWorks";
 import {RemoteWork} from "./remote-work/RemoteWork";
 import {Contacts} from "./contacts/Contacts";
+import {Footer} from "./footer/Footer";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
             <MyWorks/>
             <RemoteWork/>
             <Contacts/>
+            <Footer/>
         </div>
     );
 }
--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -3,19 +3,20 @@ import s from './Footer.module.css'
 import common from './../common/styles/Container.module.css'
 import {Instance} from "./Instance";
 
-let describe: string = `Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
-                        per inceptos himenaeos.`
-
 export const Footer = () => {
     return (
         <div className={s.footerBlock}>
             <div className={`${common.container} ${s.footerContainer}`}>
-                <h3 className={s.title}>El Chupa</h3>
+                <h3 className={s.title}>El Chupa Pupa</h3>
                 <div className={s.instances}>
 
                    <Instance/>
+                   <Instance/>
+                   <Instance/>
+                   <Instance/>
 
                 </div>
+                <span className={s.copyrights}>&copy; 2021 All rights reserved</span>
             </div>
         </div>
     )
