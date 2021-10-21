4832c6944d570b5083aaac3867bc4fe97fa0c06a
dzimentey <dzimentey@gmail.com>
2021-10-21 15:52:03 +0300

    refactor: divs instead Instance

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import s from './Footer.module.css'
 import common from './../common/styles/Container.module.css'
-import {Instance} from "./Instance";
 
 export const Footer = () => {
     return (
         <div className={s.footerBlock}>
             <div className={`${common.container} ${s.footerContainer}`}>
                 <h3 className={s.title}>El Chupo Pupo</h3>
-                <div className={s.instances}>
-
-                   <Instance/>
-                   <Instance/>
-                   <Instance/>
-                   <Instance/>
-
+                <div className={s.iconsBlock}>
+                    <div className={s.icon}></div>
+                    <div className={s.icon}></div>
+                    <div className={s.icon}></div>
+                    <div className={s.icon}></div>
                 </div>
                 <span className={s.copyrights}>&copy; 2021 All rights reserved</span>
             </div>
