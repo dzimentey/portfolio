c00fab99c11116d6213215d225744dfc7bde4dbd
dzimentey <dzimentey@gmail.com>
2021-11-11 19:16:18 +0300

    add: change title, React word added

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -7,9 +7,14 @@ export const Main = () => {
         <div className={s.mainBlock}>
             <div className={common.container}>
                 <div className={s.intro}>
-                    <span>Hello you guys. I'm</span>
-                    <h1>El Chupo Pupo</h1>
-                    <p>Different ends developer...</p>
+                    <span>Hello you guys. I'm El Chupo Pupo</span>
+                    <h1>Front-end <span>React</span> developer</h1>
+                    {/*<p>Front-end React developer</p>*/}
+                    {/*<p>There are many variations of passages of Lorem Ipsum available,*/}
+                    {/*    but the majority have suffered alteration in some form, by injected humour, or randomised words*/}
+                    {/*    which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum by*/}
+                    {/*    injected humour, or randomised words which don't look even slightly believable. If you are going*/}
+                    {/*    to use a passage of Lorem Ipsum</p>*/}
                 </div>
                 <div className={s.photo}>
                 </div>
