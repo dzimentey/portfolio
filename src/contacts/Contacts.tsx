7afe6d126a1eb2526c545e8e0a3f42cb99611700
dzimentey <dzimentey@gmail.com>
2021-11-11 19:17:24 +0300

    add: placeholder to contact form

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -6,12 +6,12 @@ export const Contacts = () => {
     return (
         <div className={s.contactsBlock}>
             <div className={`${common.container} ${s.contactsContainer}`}>
-                <h3 className={s.title}>Contacts</h3>
+                <h3 className={s.title}>Quick Contact Form</h3>
 
                     <form action="" className={s.form}>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <textarea name="" id=""  rows={8}></textarea>
+                        <input type="text" placeholder={'Your Name'}/>
+                        <input type="text" placeholder={'Your Email'}/>
+                        <textarea name="" id=""  rows={8} placeholder={'Your message'}></textarea>
                     </form>
 
 
