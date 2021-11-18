973d96b38219acee014509cdabf6bbf19f4fb7c2
dzimentey <dzimentey@gmail.com>
2021-11-18 19:04:00 +0300

    add: description to remote work block

--- src/remote-work/RemoteWork.tsx
+++ src/remote-work/RemoteWork.tsx
@@ -7,10 +7,19 @@ export const RemoteWork = () => {
         <div className={s.remoteWorkBlock}>
             <div className={`${common.container} ${s.remoteWorkContainer}`}>
 
-                    <h3 className={s.title}>Open for remote work</h3>
-                    <div className={s.hireButton}>
-                        Hire me
-                    </div>
+                <h3 className={s.title}>Open for remote work</h3>
+
+                <div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Adipisci amet architecto beatae consequuntur, earum est iste libero magnam repellendus sit?
+                        Accusamus aut dolores id incidunt ratione repudiandae? Aspernatur autem, repudiandae!
+                    </p>
+                </div>
+
+                <div className={s.hireButton}>
+                    Hire me
+                </div>
 
             </div>
         </div>
