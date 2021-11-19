585b0059ceb7ad7ba75a68e4756fb851ccb578c5
dzimentey <dzimentey@gmail.com>
2021-11-19 16:23:59 +0300

    add: Title component

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Skills.module.css'
 import common from './../common/styles/Container.module.css'
 import {Skill} from "./skill/Skill";
+import {Title} from "../common/components/title/Title";
 
 let skill1: string = `Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
                         per inceptos himenaeos.`
@@ -10,7 +11,8 @@ export const Skills = () => {
     return (
         <div className={s.skillsBlock}>
             <div className={`${common.container} ${s.skillsContainer}`}>
-                <h2 className={s.title}>Skills</h2>
+                {/*<h2 className={s.title}>Skills</h2>*/}
+                <Title text={'My skills'}/>
                 <div className={s.skills}>
                     <Skill title={'React'} description={skill1}/>
                     <Skill title={'Redux'} description={skill1}/>
